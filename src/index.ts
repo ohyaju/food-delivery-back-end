5b574684d027aebbe7362696360f1c17f9bcae04
24LP8280 24LP8280 <24LP8280@G303-CC0161.local>
2025-04-03 12:28:33 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express, { Express, Request, Response} from "express";
+import express, { Request, Response } from "express";
 
 const PORT = 8000;
 const app = express();
 
+app.use(express.json())
 
 const users = [
     {
@@ -19,16 +20,53 @@ const users = [
     },
 ]
 
-app.get("/",(_req:Request, res: Response)=>{
-    res.json({msg: "hello"});
+app.get("/", (_req: Request, res: Response) => {
+    res.json({ msg: "hello" });
 });
-app.post("/food", async (request, responce)=>{
-    console.log(request.body);
-    responce.json({success: true})
+
+app.get("/users", (_req: Request, res: Response) => {
+    res.json({ success: true, users });
 });
-app.get ("/",(_req: Request, res: Response)=>{
-    res.json({success: true, users})
-});
+
+interface UserParams {
+    id: string;
+}
+
+interface User {
+    _id: number;
+    name: string;
+    age: number;
+    tags: string[];
+}
+interface UserResponse {
+    success: boolean;
+    users?: User []| User;
+    message?: string;
+}
+app.get(
+    "/users/:id",
+    (req: Request<UserParams>, res: Response<UserResponse>) => {
+        const { id } = req.params;     //Задалж авах
+
+        const user = users.find((user) => user._id === Number(id));  //filter array буцаадаг, find object буцаадаг
+
+        res.json({ success: true, users });
+    });
+
+app.put(
+    "/users/:id",
+    (req: Request<UserParams>, res: Response<UserResponse>) => {
+        const { id } = req.params;     //Задалж авах
+        console.log(req.body);
+        users.filter((user) => {
+            if (user._id === Number(id)) { //filter array буцаадаг, find object буцаадаг
+                const updatedUser = { ...user, ...req.body };
+                user=updatedUser;
+            }
+        });
+       
+    });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running http://localhost ${PORT}`);
