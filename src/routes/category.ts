40464536f7dd6bd167197b6a69a2eeaa03716bd3
24LP8280 24LP8280 <24LP8280@G303-CC0161.local>
2025-04-14 12:33:41 +0800

    up

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { createCategory, getCategory,updateCategory,deleteCategory } from "../controller/category";
+import { createCategory, getCategory, updateCategory, deleteCategory, getCategoriesWithFoods } from "../controller/category";
 
 
 const categoryRouter = express.Router();
 categoryRouter
     .post("/", createCategory)  // POST /api/v1/category
     .get("/", getCategory)  // GET /api/v1/category
+    .get("/with-foods", getCategoriesWithFoods)
     .put("/:id", updateCategory)    // PUT /api/v1/category/:id
     .delete("/:id", deleteCategory) // DELETE /api/v1/category/:id
 
