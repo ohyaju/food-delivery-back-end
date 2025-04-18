c11a76a2849e80d37be40489c09c7740cc8e9c16
24LP8280 24LP8280 <24LP8280@G303-CC0161.local>
2025-04-18 12:12:56 +0800

    render up

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,17 @@ app.use(express.json());
 
 app.use("/api/v1/food", foodRouter);
 app.use("/api/v1/category", categoryRouter);
+ 
+let count = 0;
+app.get ("/",(req,res)=>{
+    console.log(req, count++);
 
-
+    res.send("buuz");
+});
  app.listen(PORT, async () => {
         await connection();
         console.log(`Server is running http://localhost: ${PORT}`);
 
     });
+
+
